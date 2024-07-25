0881489a0b10abb036649ac23eff582cfa240681
Roberto Vetere <Roberto.vetere@gmail.com>
2024-07-25 17:12:43 +0200

    add some styles to beautify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavegadorComponent } from './components/navegador/navegador.component';
 import { RouterOutlet } from '@angular/router';
 import { NavegadorMobileComponent } from './components/navegador-mobile/navegador-mobile.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavegadorComponent, CommonModule, NavegadorMobileComponent ],
+  imports: [RouterOutlet, NavegadorComponent, CommonModule, NavegadorMobileComponent,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
