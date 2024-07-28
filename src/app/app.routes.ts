84850c1155e8e13726445b216cc1e8a43c900054
Roberto Vetere <Roberto.vetere@gmail.com>
2024-07-28 17:05:56 +0200

    add vercel.json cors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,40 @@
-import { NgModule } from '@angular/core';
-import { HomeComponent } from './pages/home/home.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
-import { NotfoundComponent } from './pages/notfound/notfound.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+import { IntroComponent } from './pages/intro/intro.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AuthGuard } from './auth.guard';
-import { IntroComponent } from './pages/intro/intro.component';
-import { Routes, RouterModule, provideRouter, withPreloading, PreloadAllModules  } from '@angular/router';
 
-
-
-export const routes: Routes = [
-  { path: '', component: IntroComponent, pathMatch: 'full'},
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+// Define las rutas
+const routes: Routes = [
+  { path: '', component: IntroComponent, pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent),
+    canActivate: [AuthGuard]
+  },
   { path: 'login', component: LoginComponent },
-  { path: 'gallery', component: GalleryComponent, canActivate: [AuthGuard] },
+  {
+    path: 'gallery',
+    loadComponent: () => import('./pages/gallery/gallery.component').then(m => m.GalleryComponent),
+    canActivate: [AuthGuard]
+  },
   { path: '**', redirectTo: 'home' }
 ];
+
+// Configura el proveedor de rutas
+const appConfig = {
+  providers: [
+    provideRouter(routes)
+  ]
+};
+
+// Inicia la aplicación
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
